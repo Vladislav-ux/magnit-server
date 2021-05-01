29f3bc91204c2300cef6c61fd9110e608848ed7e
Vladislav Vekshin <vladik201518@gmail.com>
2021-05-01 21:04:21 +0300

    comment authorization

--- src/main/java/ru/magnit/demo/auth/SecurityConfig.java
+++ src/main/java/ru/magnit/demo/auth/SecurityConfig.java
@@ -1,17 +1,17 @@
-package ru.magnit.demo.auth;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity security) throws Exception {
-        //Disable Spring's basic security settings as they are not relevant for this sample
-        security.httpBasic().disable();
-    }
-}
+//package ru.magnit.demo.auth;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity security) throws Exception {
+//        //Disable Spring's basic security settings as they are not relevant for this sample
+//        security.httpBasic().disable();
+//    }
+//}
